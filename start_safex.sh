26316385d4b43c97d17c1285f8a9a1875f5732cd
prakshitvjain <prakshit17036@gmail.com>
2025-10-08 22:16:25 +0530

    Improvements and ASCII art

--- start_safex.sh
+++ start_safex.sh
@@ -1,5 +1,26 @@
 #!/bin/bash
-sudo systemctl start httpd
-sudo systemctl start mariadb
-x=$(ip -4 addr show scope global | grep inet | awk '{print $2}' | cut -d/ -f1)
-echo "The services are successfully started, open any browser and type $x/login.php"
+cat << "EOF"
+ ____         __     __  __ __     __          _ _   
+/ ___|  __ _ / _| ___\ \/ / \ \   / /_ _ _   _| | |_ 
+\___ \ / _` | |_ / _ \\  /   \ \ / / _` | | | | | __|
+ ___) | (_| |  _|  __//  \    \ V / (_| | |_| | | |_ 
+|____/ \__,_|_|  \___/_/\_\    \_/ \__,_|\__,_|_|\__|
+                                                     
+EOF
+sleep 1
+if [ "$(id -u)" -ne 0 ]; then
+  echo "This command requires root privileges, kindly run as root or with sudo"
+  exit 1
+else
+  echo "Starting services..."
+  sleep 1
+  sudo systemctl start httpd
+  sudo systemctl start mariadb
+  x=$(ip -4 addr show scope global | grep inet | awk '{print $2}' | cut -d/ -f1)
+  echo "Services successfully started"
+  echo "The project is accessible at URL http://$x/login.php"
+fi
+
+
+
+
